ccbf54743509316f766068f34db90d61fbed20fa
冯勇 <2462345496@qq.com>
2020-01-02 17:09:18 +0800

    添加首页内容

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,28 @@
 <template>
 	<view>
+		<view class="home-box">
+			<swiper class="screen-swiper square-dot" :indicator-dots="true" :circular="true"
+			 :autoplay="true" interval="5000" duration="500">
+				<swiper-item v-for="(item,index) in swiperList" :key="index">
+					<image :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
+					<video :src="item.url" autoplay loop muted :show-play-btn="false" :controls="false" objectFit="cover" v-if="item.type=='video'"></video>
+				</swiper-item>
+			</swiper>
+			
+			<view class="cu-list grid col-3 no-border">
+				<view class="cu-item" v-for="(item,index) in cuIconList" :key="index">
+					<view :class="['cuIcon-' + item.cuIcon,'text-' + item.color]">
+						<view class="cu-tag badge" v-if="item.badge!=0">
+							<block v-if="item.badge!=1">{{item.badge>99?'99+':item.badge}}</block>
+						</view>
+					</view>
+					<text>{{item.name}}</text>
+				</view>
+			</view>
+		</view>
+		
+		
+		
 		<view class="box">
 			<view class="cu-bar tabbar bg-white shadow foot">
 				<view class="action" :class="{'text-gray' : curNavId!=0,  'text-green' : curNavId==0}" @click="NavChange" data-navId="0" >
@@ -34,6 +57,68 @@
 		data() {
 			return {
 				curNavId: -1,
+				
+				swiperList: [{
+					id: 0,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+				}, {
+					id: 1,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big37006.jpg',
+				}, {
+					id: 2,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+				}, {
+					id: 3,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+				}, {
+					id: 4,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+				}, {
+					id: 5,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+				}, {
+					id: 6,
+					type: 'image',
+					url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+				}],
+				
+				cuIconList: [{
+					cuIcon: 'cardboardfill',
+					color: 'red',
+					badge: 120,
+					name: '日历'
+				}, {
+					cuIcon: 'recordfill',
+					color: 'orange',
+					badge: 1,
+					name: '天气'
+				}, {
+					cuIcon: 'picfill',
+					color: 'yellow',
+					badge: 0,
+					name: '动态'
+				}, {
+					cuIcon: 'noticefill',
+					color: 'olive',
+					badge: 22,
+					name: '通知'
+				}, {
+					cuIcon: 'upstagefill',
+					color: 'cyan',
+					badge: 0,
+					name: '排行榜'
+				}, {
+					cuIcon: 'clothesfill',
+					color: 'blue',
+					badge: 0,
+					name: '皮肤'
+				}],
 			}
 		},
 		methods: {
@@ -42,6 +127,12 @@
 			 */
 			NavChange: function(e) {
 				this.curNavId=parseInt(e.currentTarget.dataset.navid);
+				if(this.curNavId==0)
+				{
+					uni.navigateTo({
+						url: '/pages/demo/index'
+					})
+				}
 			}
 		}
 	}
